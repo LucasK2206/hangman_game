8133675972e328c4244a082a8883f32d722e7232
Lukasz <bleyosss@wp.pl>
2022-08-13 13:44:04 +0200

    JS update

--- hangman_app.js
+++ hangman_app.js
@@ -2,10 +2,13 @@ const userLetter = document.querySelector("#user-data");
 const errorCode = document.querySelector("#error-code");
 const availableLettersBox = document.querySelector("#availableLetters");
 const usedLetters = document.querySelector("#usedLetters");
+const gamePasswdBox = document.querySelector("#game__display--passwd");
 
 const reg = new RegExp("^[a-z]*$");
 
-let str = "";
+let availableLettersAll = [];
+
+const passwd = "ala ma kota";
 const allLetters = "abcdefghijklmnopqrstuvwxyz";
 const tabOfAllLetters = [...allLetters];
 
@@ -15,6 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 })
 const initGame = function(){
+    hashPasswd();
     addavailableLetters();
     getDataFromUser();
 
@@ -27,7 +31,6 @@ const getDataFromUser = function(){
             errorCode.textContent = "Podaj literę z przedziału A-Z, a-z";
             console.log(singleLetter);
             addavailableLetters(singleLetter);
-            singleLetter = "";
         }else {
             singleLetter = "";
             console.log(errorCode);
@@ -37,13 +40,31 @@ const getDataFromUser = function(){
 }
 const addavailableLetters = function(singleLetter) {
     if(singleLetter){
-
+        availableLettersAll.forEach((value, index) => {
+            if(singleLetter === value){
+                availableLettersAll.splice(index, 1);
+                availableLettersBox.textContent = availableLettersAll.join(" ");
+                usedLetters.textContent += value + " ";
+            }
+        })
     }
     else {
         tabOfAllLetters.forEach( availableLetter => {
-            str+=availableLetter + " ";
-            availableLettersBox.textContent = str;
-            return str;
+            availableLettersAll.push(availableLetter);
+
+            availableLettersBox.textContent = availableLettersAll.join(" ");
+        })
+    }
+}
+const hashPasswd = function() {
+    let passwd1 = "";
+    [...passwd].forEach(letter => {
+        if(letter !== " "){
+            passwd1+= "-"
+        }else {
+            passwd1+=" ";
         }
     })
+
+    gamePasswdBox.textContent = passwd1;
 }
