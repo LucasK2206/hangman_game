3ba38d3bcd3f9302080b852306e27ea10e80ef85
Lukasz <bleyosss@wp.pl>
2022-08-13 12:52:06 +0200

    basic JS added

--- hangman_app.js
+++ hangman_app.js
@@ -1,5 +1,49 @@
-const userData = document.querySelector("#user-data");
+const userLetter = document.querySelector("#user-data");
+const errorCode = document.querySelector("#error-code");
+const availableLettersBox = document.querySelector("#availableLetters");
+const usedLetters = document.querySelector("#usedLetters");
 
-userData.addEventListener("change", (x) => {
-    console.log(x.target.value);
+const reg = new RegExp("^[a-z]*$");
+
+let str = "";
+const allLetters = "abcdefghijklmnopqrstuvwxyz";
+const tabOfAllLetters = [...allLetters];
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    initGame();
+
 })
+const initGame = function(){
+    addavailableLetters();
+    getDataFromUser();
+
+}
+
+const getDataFromUser = function(){
+    userLetter.addEventListener("change", (letter) => {
+        let singleLetter = letter.target.value.toLowerCase();
+        if(reg.test(singleLetter) == true && singleLetter !== "" && singleLetter !== " "){
+            errorCode.textContent = "Podaj literę z przedziału A-Z, a-z";
+            console.log(singleLetter);
+            addavailableLetters(singleLetter);
+            singleLetter = "";
+        }else {
+            singleLetter = "";
+            console.log(errorCode);
+            errorCode.textContent = " Błąd: Podaj literę z przedziału A-Z, a-z";
+        }
+    })
+}
+const addavailableLetters = function(singleLetter) {
+    if(singleLetter){
+
+    }
+    else {
+        tabOfAllLetters.forEach( availableLetter => {
+            str+=availableLetter + " ";
+            availableLettersBox.textContent = str;
+            return str;
+        }
+    })
+}
