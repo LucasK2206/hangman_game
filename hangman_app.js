37622a7442672b6536b1d68292ff945db8a3cc5f
Lukasz <bleyosss@wp.pl>
2022-08-15 13:51:26 +0200

    adding js functionality and css styles and changing the html structure

--- hangman_app.js
+++ hangman_app.js
@@ -1,10 +1,15 @@
 import { passwordObj } from './passwordsObj.js'
 
-const userLetter = document.querySelector("#user-letter");
-const errorCode = document.querySelector("#error-code");
-const availableLettersBox = document.querySelector("#availableLetters");
-const usedLetters = document.querySelector("#usedLetters");
-const gamePasswdBox = document.querySelector("#game__display--passwd");
+const userLetter = document.querySelector(".user-letter");
+const errorCode = document.querySelector(".error-code");
+const availableLettersBox = document.querySelector(".available-letters");
+const usedLetters = document.querySelector(".used-letters");
+const gamePasswdBox = document.querySelector(".game__display--passwd");
+const gameBoardContent = document.querySelector(".chance-left");
+const gameOver = document.querySelector(".game-over");
+const endScreenDisp = document.querySelector(".game-passwd-disp");
+const endScreenPasswd = document.querySelector(".game-result");
+const resetGameBtn = document.querySelector(".reset-game");
 
 const reg = new RegExp("^[a-z]*$");
 
@@ -24,8 +29,10 @@ const initGame = function(){
     hashPasswd();
     addavailableLetters();
     getDataFromUser();
+    resetGame();
 }
 const drawPassword = function() {
+    gameBoardContent.innerText = numberOfChances;
     let minNbOfPasswds = 1;
     let maxNbOfPasswds = Object.keys(passwordObj)[Object.keys(passwordObj).length-1];
     console.log(maxNbOfPasswds)
@@ -87,6 +94,7 @@ const checkLetterInPasswd = function(singleUserLetter) {
     })
     if(passwdCopyCheacker.join("") === passwdCopy.join("")){
         numberOfChances--;
+        gameBoardContent.innerText = numberOfChances;
     }
     console.log(numberOfChances);
 
@@ -97,7 +105,19 @@ const checkLetterInPasswd = function(singleUserLetter) {
 const passwordComplianceChecker = function() {
     if(passwd.trim() === passwdCopy.join("").trim()){
         console.log("Wygrana!");
+        gameOver.classList.toggle("game-disabled");
+        endScreenDisp.textContent = "Wygrana! \n hasło to:";
+        endScreenPasswd.textContent = passwd;
+
     } else if(numberOfChances === 0){
-        console.log("Przegrana")
+        console.log("Przegrana");
+        endScreenDisp.textContent = "Przegrana :/ \n hasło to:"
+        endScreenPasswd.textContent = passwd;
+        gameOver.classList.toggle("game-disabled");
     }
+}
+const resetGame = function() {
+    resetGameBtn.addEventListener("click", () => {
+        location.reload();
+    })
 }
